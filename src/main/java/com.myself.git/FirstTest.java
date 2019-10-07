21bd9a925d1472b24c9874f90425f3debc957380
wangrong <876633930@qq.com>
2019-10-07 19:31:02 +0800

    提交idea开发git第一个测试程序

---
+++ src/main/java/com.myself.git/FirstTest.java
@@ -0,0 +1,12 @@
+package com.myself.git;
+
+/**
+ * @Auther wangrong
+ * @Date 2019/10/7 19:23
+ * @Description
+ */
+public class FirstTest {
+    public static void main(String[] args) {
+        System.out.println("hello test");
+    }
+}
